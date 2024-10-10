597e7e7229ef0f4ec98adeee6588e8faf49bb0ce
astn <github@msg.austinh.io>
2024-10-10 09:10:30 -0700

    Done

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const btnSubmitFeedback = document.getElementById('btn-rating-submit');
+const sectionRating = document.querySelector('.section-rate');
+const sectionThankYou = document.querySelector('.section-thank-you');
 
 function updateRatingLabel() {
   const ratingLabel = document.getElementById('rating-label');
@@ -6,6 +8,9 @@ function updateRatingLabel() {
 
   if (selectedRating) {
     ratingLabel.innerText = selectedRating.value;
+    sectionThankYou.style.visibility = 'visible';
+    sectionRating.style.transform = 'translateX(-100%)';
+    sectionThankYou.style.transform = 'translateX(0%)';
   }
 }
 
